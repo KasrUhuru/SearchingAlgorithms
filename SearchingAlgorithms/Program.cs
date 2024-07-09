28bd265bf048805699ee48bea96371c378df591c
MRivera <matias.g.rivera.civ@gmail.com>
2024-07-09 15:12:26 -0700

    Add project files.

---
+++ SearchingAlgorithms/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class SearchingAlgorithms
+{
+    static void Main(string[] args)
+    {
+        int[] arr1 = { 1, 1, 2, 2,2, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9, 10 };
+
+        int arr1Result = LinearSearch(arr1, 4);
+        Console.WriteLine(arr1Result);
+    }
+
+    public static int LinearSearch(int[] arrayToSearch, int target)
+    {
+        // Initialize the loop conditiional to 0 in this case
+        // Next is to determine if the value satisfies the condition
+        // After each loop we run the update logic (i-- also works as well as i++)
+        for (int i = 0; i < arrayToSearch.Length; i++)
+        {
+            if (arrayToSearch[i] == target) return i;
+        }
+
+        return -1;
+    }
+}
---
+++ SearchingAlgorithmsTest/ProgramTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void LinearSearch_TargetIsNotInArray_ShouldReturnNegativeOne()
+        {
+            // Consider the Triple A's of testing: Arrange, Act, Assert
+            // Arrange: Setting up the mock-ups
+            int[] arr1 = { 1, 1, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9, 10 };
+            // Act: Call the code you want to test
+            int arr1Result = SearchingAlgorithms.LinearSearch(arr1, 16);
+            // Assert: This is the outcome that you expect. The hypothesis to be tested
+            Assert.AreEqual(arr1Result, -1);
+        }
+
+        [TestMethod()]
+        public void LinearSearch_TargetIsNotInArray_ShouldReturnValidIndex()
+        {
+            // Consider the Triple A's of testing: Arrange, Act, Assert
+            // Arrange: Setting up the mock-ups
+            int[] arr1 = { 1, 1, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9, 10 };
+            // Act: Call the code you want to test
+            int arr1Result = SearchingAlgorithms.LinearSearch(arr1, 3);
+            // Assert: This is the outcome that you expect. The hypothesis to be tested
+            Assert.AreEqual(arr1Result, 7);
+        }
+    }
+}
